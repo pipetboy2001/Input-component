df49d4d4e969e8c1fdbe8577a93d6389906662f6
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-12-30 22:10:41 -0300

    Fix: Input icon funciona y arreglos en grilla

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,28 +1,49 @@
+import React, { Fragment } from 'react';
 import classnames from 'classnames';
 import './../Styles/Input.css';
 
-export default function Input(props) {
-    const { error, disabled, helperText, icon,iconPosition, size, color, fullWidth, multiline ,maxRows} = props;
+const Input = ({ variant, error, disabled, helperText, icon, iconPosition, size, color, fullWidth, multiline, maxRows, children, ...props }) => {
     const className = classnames(
         'input',
-        error && 'error',
-        disabled && 'disabled',
-        size && `size-${size}`,
-        color && `color-${color}`,
-        fullWidth && 'full-width',
-        multiline && 'multiline',
-        icon && `icon-${icon}`
+        variant && `input-${variant}`,
+        {
+            'input-error': error,
+            'input-disabled': disabled,
+            'input-sm': size === 'sm',
+            'input-md': size === 'md',
+            'input-lg': size === 'lg',
+            'input-primary': color === 'primary',
+            'input-secondary': color === 'secondary',
+            'input-full-width': fullWidth,
+            'input-multiline': multiline,
+        }
     );
 
     return (
-        <>
-            {multiline ? (
-                <textarea className={className} maxRows={maxRows} {...props} />
-            ) : (
-                <input className={className} {...props} />
+        <Fragment>
+            {icon && (
+                <Fragment>
+                    {iconPosition === 'left' && icon}
+                    {multiline ? (
+                        <textarea className={className} maxRows={maxRows} {...props} />
+                    ) : (
+                        <input className={className} {...props} />
+                    )}
+                    {iconPosition === 'right' && icon}
+                </Fragment>
+            )}
+            {!icon && (
+                <Fragment>
+                    {multiline ? (
+                        <textarea className={className} maxRows={maxRows} {...props} />
+                    ) : (
+                        <input className={className} {...props} />
+                    )}
+                    {children}
+                </Fragment>
             )}
             {helperText && <div className="helper-text">{helperText}</div>}
-            
-        </>
+        </Fragment>
     );
-}
+};
+export default Input;
--- src/Components/PageImput.js
+++ src/Components/PageImput.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import  Input  from './Input';
+import Input from './../Components/Input'
+
 import { FaBeer } from 'react-icons/fa';
+import { BiError } from 'react-icons/bi';
+import { MdDisabledByDefault } from 'react-icons/md';
+import { FaHireAHelper } from 'react-icons/fa';
+import { GiResize } from 'react-icons/gi';
+import { CgDisplayFullwidth } from 'react-icons/cg';
+import { BsInputCursorText } from 'react-icons/bs';
 
 import './../Styles/PageInput.css'
 
@@ -8,50 +15,49 @@ export const PageImput = () => {
     return (
         <>
             <div className='PageInputContainer'>
-                <h1>Inputs <FaBeer/> </h1>
+                <h1>Inputs <BsInputCursorText/> </h1>
                 <div>
                     <div className='InputBasicos'>
-                        <h2>Default</h2>
-                        <Input />
-                        <h2>error</h2>
-                        <Input error={true} />
-                        <h2>disabled</h2>
-                        <Input disabled={true} /> 
+                        <h2>Default </h2>
+                        <Input placeholder="Input Default"/>
+                        <h2>Error <BiError/> </h2>
+                        <Input error={true} placeholder="Input Error" />
+                        <h2>Disabled <MdDisabledByDefault/></h2>
+                        <Input disabled={true} placeholder="Input Desahbilitado" /> 
                     </div>
 
                     <div className='InputHelper'>
-                        <h2>helperText</h2>
-                        <Input helperText="Este es un ejemplo de texto de ayuda" />
-                        <Input helperText="Este es un ejemplo de texto de ayuda" />
-                        <Input helperText="Este es un ejemplo de texto de ayuda" />
+                        <h2>HelperText </h2>
+                        <Input helperText="Este es un ejemplo de texto de ayuda" placeholder="Input Helper" />
+                        <Input helperText="Este es un ejemplo de texto de ayuda" placeholder="Input Helper" />
+                        <Input helperText="Este es un ejemplo de texto de ayuda" placeholder="Input Helper" />
                     </div>
 
                     <div className='InputIcons'>
-                        <h2>icon</h2>
-                        <Input icon={<FaBeer />} iconPosition="left" />
-                        <Input icon={<FaBeer />} iconPosition="right" />
-                        <Input icon={<FaBeer />} iconPosition="left" />
+                        <h2>Icon <FaBeer /> </h2>
+                        <Input icon={<FaBeer />} iconPosition="left" placeholder="Input Icon" />
+                        <Input icon={<FaBeer />} iconPosition="right" placeholder="Input Icon" />
                         
                     </div>
                     
                     <div className='inputSize'>
-                        <h2>size</h2>
-                        <Input size="sm" />
-                        <Input size="md" />
-                        <Input size="lg" />
-                        <h2>fullWidth</h2>
-                        <Input fullWidth={true} />
+                        <h2>Size <GiResize/></h2>
+                        <Input size="sm" placeholder="Input Pequeño" />
+                        <Input size="md" placeholder="Input Mediano" />
+                        <Input size="lg" placeholder="Input Grande" />
+                        <h2>fullWidth <CgDisplayFullwidth/></h2>
+                        <Input fullWidth={true} placeholder="Input full tamaño" />
                     </div>
 
                     <div className='inputColor'>
-                        <h2>color</h2>
-                        <Input color="primary" />
-                        <Input color="secondary" />
+                        <h2>Color</h2>
+                        <Input color="primary" placeholder="Input color primario" />
+                        <Input color="secondary" placeholder="Input color secudandario" />
                     </div>
 
                     <div className='inputMultiline'>
-                        <h2>multiline</h2>
-                        <Input multiline={true} maxRows={4} />
+                        <h2>Multiline</h2>
+                        <Input multiline={true} maxRows={4} placeholder="Input multilinea" />
                     </div>
                 </div>
             </div>
