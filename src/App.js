c9973f935f78430a99f10a2bb91217ee0e5d630e
Felipe Fernandez <FelipeFelizx@gmail.com>
2023-01-01 19:04:21 -0300

    Feature: Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { PageImput } from './Components/PageImput'
+import  {Footer}  from './Components/Footer'
 
 function App() {
   return (
     <div className="App">
       <PageImput />
+      <Footer />
+
     </div>
   );
 }
